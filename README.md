b5274358fbf76c877e02351ffa108a9c8afc51d9
Hyebin <gpq8l8s@gmail.com>
2022-03-15 15:53:01 +0100

    modified css

--- README.md
+++ README.md
@@ -6,10 +6,10 @@
 
 - Finished for the first on October 2021
 - Finished for the seconde time on December 2021 : made code cleaner (less div, span / reduced unnecessarily big size of the photos ...)
-- <em>Janv. ~ Current</em> : rebuilding with SASS to make site more organised, and responsive.
+- <em>Janv. ~ Mar</em> : rebuilding with SASS to make site more organised, and responsive.
 
 ### Used funtions / Effects
 
-- ` ::-webkit-scrollbar`
-
-- `carousel()`
+- Carousel
+- Transform / left on header
+- Overflow to control whitespace on the browser ect.
---
+++ theme.js
@@ -0,0 +1,42 @@
+'use strict';
+
+//function nightDayHandler(theme) {
+//  const target = document.querySelector('body');
+//  if (theme.value === 'night') {
+//    target.style.backgroundColor = 'black';
+//    target.style.color = 'white';
+//    theme.value = 'day';
+//
+//    let list = document.querySelectorAll('.titleLogo, ul.nav_menu > li');
+//    let i = 0;
+//    while (i < list.length) {
+//      list[i].style.color = 'white';
+//      i++;
+//    }
+//  } else {
+//    target.style.backgroundColor = 'white';
+//    target.style.color = 'black';
+//    theme.value = 'night';
+//
+//    let list = document.querySelectorAll('.titleLogo, .nav_menu');
+//    let i = 0;
+//    while (i < list.length) {
+//      list[i].style.color = 'black';
+//      i++;
+//    }
+//  }
+//}
+
+//const mode = document.querySelectorAll("div.nav_menu");
+//
+//function changeMode() {
+//  if (document.querySelector('#change_mode_dark').value === 'night'){
+//    document.querySelector('body').style.backgroundColor = 'black';
+//    document.querySelector(mode).style.color = 'white';
+//    document.querySelector('#change_mode_dark').value = 'day';
+//  } else {
+//    document.querySelector('body').style.backgroundColor = 'white'
+//    document.querySelectorAll('div.nav_menu').style.color = 'black';
+//    document.querySelector('#change_mode_light').value = 'night'
+//  }
+//}
